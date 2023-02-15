8fd88abf71589c7b4789adb36c0d0fce5e8918b5
epaz <emiliocambados@gmail.com>
2023-02-15 16:31:11 +0100

    Initial commit

---
+++ src/test/java/com/example/deploy/test_deploy/TestDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploy.test_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
